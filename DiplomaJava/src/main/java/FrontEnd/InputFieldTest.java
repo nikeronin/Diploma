43f0f20a94c89964338a3e7722e350eed5d36a7b
nikeronin <eronin.crm@gmail.com>
2026-01-16 02:00:36 +0500

    first import and change selenide version

--- DiplomaJava/src/main/java/FrontEnd/InputFieldTest.java
+++ DiplomaJava/src/main/java/FrontEnd/InputFieldTest.java
@@ -1,4 +1,17 @@
 package FrontEnd;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class InputFieldTest {
+
+    SelenideElement TextInput = $(By.name("text"));
+
 }
